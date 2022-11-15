9a0c21cc9e1a70fd18fabaa00619c23741dc90a2
Riwa <riwa.hoteit@gmail.com>
2022-11-15 20:35:16 +0200

    Cleaning Up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Canvas } from "@react-three/fiber";
-import { Suspense, useState, useEffect } from "react";
+import { Suspense, useState, useEffect, useCallback } from "react";
 import { useGLTF, Environment, OrbitControls, ContactShadows, SpotLight } from "@react-three/drei";
 import { proxy, useSnapshot} from 'valtio'
 import { HexColorPicker } from "react-colorful";
@@ -8,7 +8,7 @@ import { HexColorPicker } from "react-colorful";
 const state = proxy({
   current : null,
   items : {
-    table: "#ff0000",
+    shoe: "#ff0000",
   }
 })
 
@@ -16,30 +16,46 @@ const state = proxy({
 function Shoe(props) {
   const snap = useSnapshot(state)
   const { nodes, materials } = useGLTF('/shoe.gltf')
-  const [ hovered, set ] = useState(null)
+  const [ hovered ] = useState(null)
+  const [ meshes, setMeshes ] = useState(() => [
+    { id: "laces", geometry: nodes.shoe_7.geometry, material: materials.laces, materialColor: snap.items.laces },
+    { id: "inner", geometry: nodes.shoe_6.geometry, material: materials.inner, materialColor: snap.items.inner },
+    { id: "patch", geometry: nodes.shoe_5.geometry, material: materials.patch, materialColor: snap.items.patch },
+    { id: "caps", geometry: nodes.shoe_4.geometry, material: materials.caps, materialColor: snap.items.caps },
+    { id: "band", geometry: nodes.shoe_3.geometry, material: materials.band, materialColor: snap.items.band },
+    { id: "stripes", geometry: nodes.shoe_2.geometry, material: materials.stripes, materialColor: snap.items.stripes },
+    { id: "sole", geometry: nodes.shoe_1.geometry, material: materials.sole, materialColor: snap.items.sole },
+    { id: "mesh", geometry: nodes.shoe.geometry, material: materials.mesh, materialColor: snap.items.mesh },
+  ])
+
 
   useEffect(() => {
+    meshes.forEach((mesh) => {
+      mesh.material.color.set(snap.items[mesh.id])
+    })
     const cursor = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0)"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><g filter="url(#filter0_d)"><path d="M29.5 47C39.165 47 47 39.165 47 29.5S39.165 12 29.5 12 12 19.835 12 29.5 19.835 47 29.5 47z" fill="${snap.items[hovered]}"/></g><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/><text fill="#000" style="white-space:pre" font-family="Inter var, sans-serif" font-size="10" letter-spacing="-.01em"><tspan x="35" y="63">${hovered}</tspan></text></g><defs><clipPath id="clip0"><path fill="#fff" d="M0 0h64v64H0z"/></clipPath><filter id="filter0_d" x="6" y="8" width="47" height="47" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/><feOffset dy="2"/><feGaussianBlur stdDeviation="3"/><feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0"/><feBlend in2="BackgroundImageFix" result="effect1_dropShadow"/><feBlend in="SourceGraphic" in2="effect1_dropShadow" result="shape"/></filter></defs></svg>`
     const auto = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/></svg>`
     document.body.style.cursor = `url('data:image/svg+xml;base64,${btoa(hovered ? cursor : auto)}'), auto`
-  }, [hovered])
+  }, [snap.items, snap.color, hovered])
+
+  
 
   return (
+    <>
     <group {...props} dispose={null}
-    onPointerOver={(e) => (e.stopPropagation(), set(e.object.material.name))}
-    onPointerOut = {(e) => (e.intersections.length===0 && set(null))}
-    onPointerDown = {(e) => (e.stopPropagation(), state.current = e.object.material.name)}
-    onPointerMissed = {(e) => (state.current = null)}
+      onPointerDown = {(e) => (e.stopPropagation(), state.current = e.object.material.name)}
+      onPointerMissed = {(e) => (state.current = null)}
     >
-      <mesh geometry={nodes.shoe.geometry} material={materials.laces} material-color={snap.items.laces} />
-      <mesh geometry={nodes.shoe_1.geometry} material={materials.mesh} material-color={snap.items.mesh} />
-      <mesh geometry={nodes.shoe_2.geometry} material={materials.caps} material-color={snap.items.caps} />
-      <mesh geometry={nodes.shoe_3.geometry} material={materials.inner} material-color={snap.items.inner} />
-      <mesh geometry={nodes.shoe_4.geometry} material={materials.sole} material-color={snap.items.sole} />
-      <mesh geometry={nodes.shoe_5.geometry} material={materials.stripes} material-color={snap.items.stripes} />
-      <mesh geometry={nodes.shoe_6.geometry} material={materials.band} material-color={snap.items.band} />
-      <mesh geometry={nodes.shoe_7.geometry} material={materials.patch} material-color={snap.items.patch} />
+      {meshes.map((mesh) => (
+        <mesh
+          key={mesh.id}
+          geometry={mesh.geometry}
+          material={mesh.material}
+          materialColor={mesh.materialColor}
+        />
+      ))}
     </group>
+    </>
   )
 }
 
